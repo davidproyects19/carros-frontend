f55dc6e8dd63e2c387ded17dc0a9b35829b62bad
DAVID <fernandez.davidgoza@gmail.com>
2024-09-24 22:01:44 -0500

    Carga Inicial del proyecto frontend Carros

---
+++ src/main/java/pe/edu/cibertec/carros_frontend/CarrosFrontendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.carros_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrosFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrosFrontendApplication.class, args);
+	}
+
+}
